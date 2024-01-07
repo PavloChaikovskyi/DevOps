ac8a1d37e6f78698c4ddd24ede3a912919a7f227
PavloChaikovskyi <chaykovskiypavel@gmail.com>
2024-01-07 20:36:40 +0100

    feat: update security group for outbound traffic

--- TASK1/task1.tf
+++ TASK1/task1.tf
@@ -10,24 +10,35 @@ resource "aws_instance" "my_Ubuntu" {
   tags = {
     Name = "Ubuntu"
   }
+
+  # add ssh_key to instance 
   key_name = aws_key_pair.ssh_key.key_name
-  # Associate the instance with the security group
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+
+  # Associate the instance with the security group for SSH connection
+  vpc_security_group_ids = [aws_security_group.allow_ssh_and_http_https.id]
 }
 
+# init ssh_key
 resource "aws_key_pair" "ssh_key" {
   key_name   = "ssh_key"
   public_key = file("~/.ssh/id_rsa.pub")
 }
 
-resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh"
-  description = "Allow incoming SSH traffic"
-  
+resource "aws_security_group" "allow_ssh_and_http_https" {
+  name        = "allow_ssh_and_http_https"
+  description = "Allow incoming SSH and outgoing HTTP/HTTPS traffic"
+
   ingress {
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]  # Allow SSH traffic from any source
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"  # Allow all outbound traffic
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
