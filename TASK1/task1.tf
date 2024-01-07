e1a073ccca9044a597aaa6697d189b2dd81398d6
PavloChaikovskyi <chaykovskiypavel@gmail.com>
2024-01-07 16:46:21 +0100

    feat: create aws ec2 ubuntu instance with ssh connection and security group

--- TASK1/task1.tf
+++ TASK1/task1.tf
@@ -8,6 +8,26 @@ resource "aws_instance" "my_Ubuntu" {
   ami = "ami-0faab6bdbac9486fb"
   instance_type = "t2.micro"
   tags = {
-    Name = "ubuntu"
+    Name = "Ubuntu"
+  }
+  key_name = aws_key_pair.ssh_key.key_name
+  # Associate the instance with the security group
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+}
+
+resource "aws_key_pair" "ssh_key" {
+  key_name   = "ssh_key"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow incoming SSH traffic"
+  
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Allow SSH traffic from any source
   }
 }
