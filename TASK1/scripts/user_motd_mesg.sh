37f2289e7e3b0de009ff5ac33ef87e5479cd079b
PavloChaikovskyi <chaykovskiypavel@gmail.com>
2024-01-13 00:41:54 +0100

    add new motd file

--- TASK1/scripts/user_motd_mesg.sh
+++ TASK1/scripts/user_motd_mesg.sh
@@ -1,3 +1,3 @@
-echo "Hello dear user" | sudo tee /etc/update-motd.d/99-custom-motd
+echo "#!/bin/sh" | sudo tee /etc/update-motd.d/99-custom-motd
+echo 'echo "Custom message! Hello dear user"' | sudo tee -a /etc/update-motd.d/99-custom-motd
 sudo chmod +x /etc/update-motd.d/99-custom-motd
-sudo systemctl restart ssh
