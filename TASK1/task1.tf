0a3ffe1fcbd7722a5b1f9c8926007b532a878e97
PavloChaikovskyi <chaykovskiypavel@gmail.com>
2024-01-16 10:59:24 +0100

    format the code

--- TASK1/task1.tf
+++ TASK1/task1.tf
@@ -1,5 +1,8 @@
-# aws user credentials: should be hidden
+###################################################################################################
+### EC2 INSTANCE UBUNTU 
+###################################################################################################
 
+# aws user credentials: should be hidden
 provider "aws" {
 
 }
@@ -9,14 +12,14 @@ provider "aws" {
 resource "aws_instance" "my_Ubuntu" {
   ami                    = "ami-0faab6bdbac9486fb"
   instance_type          = "t2.micro"
-  tags = { Name          = "Ubuntu" }
+  tags                   = { Name = "Ubuntu" }
   key_name               = aws_key_pair.ssh_key.key_name
   vpc_security_group_ids = [aws_security_group.allow_ssh_and_http_https.id]
-  
+
   # Provisioner to execute remote script after instance will be installed
   provisioner "file" {
-    source               = "scripts/01-custom"
-    destination          = "/tmp/01-custom"
+    source      = "scripts/01-custom"
+    destination = "/tmp/01-custom"
   }
 
   provisioner "remote-exec" {
@@ -45,10 +48,10 @@ resource "aws_instance" "my_Ubuntu" {
   }
 
   connection {
-      type        = "ssh"
-      user        = "ubuntu"
-      private_key = file("~/.ssh/id_rsa")
-      host        = self.public_ip
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file("~/.ssh/id_rsa")
+    host        = self.public_ip
   }
 }
 
@@ -66,13 +69,13 @@ resource "aws_security_group" "allow_ssh_and_http_https" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Allow SSH traffic from any source
+    cidr_blocks = ["0.0.0.0/0"] # Allow SSH traffic from any source
   }
 
   egress {
     from_port   = 0
     to_port     = 0
-    protocol    = "-1"  # Allow all outbound traffic
+    protocol    = "-1" # Allow all outbound traffic
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
