aed828fecba204621b5f1fa2c65456b679d7e776
PavloChaikovskyi <chaykovskiypavel@gmail.com>
2024-01-24 00:51:16 +0100

    --allow-empty

--- TASK2/main.tf
+++ TASK2/main.tf
@@ -1,5 +1,9 @@
 # aws user credentials: should be hidden
-provider "aws" {}
+provider "aws" {
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region     = var.aws_region
+}
 
 ###################################################################################################
 ### EC2 INSTANCES
@@ -12,40 +16,57 @@ resource "aws_instance" "my_Ubuntu" {
   key_name               = aws_key_pair.ssh_key.key_name
   vpc_security_group_ids = [aws_security_group.allow_ssh_and_http_https.id]
   # Provisioner to execute remote script after instance will be installed
-  provisioner "file" {
-    source      = "scripts/01-custom"
-    destination = "/tmp/01-custom"
-  }
+  # provisioner "file" {
+  #   source      = "scripts/01-custom"
+  #   destination = "/tmp/01-custom"
+  # }
   #copy Dockerfile to instance
   provisioner "file" {
     source      = "scripts/Dockerfile"
     destination = "/home/ubuntu/Dockerfile"
   }
+  provisioner "file" {
+    source      = "scripts/backup_script.sh"
+    destination = "/home/ubuntu/backup_script.sh"
+  }
   provisioner "remote-exec" {
-    inline = ["sudo mv /tmp/01-custom /etc/update-motd.d/01-custom"]
+    inline = [
+      # "sudo mv /tmp/01-custom /etc/update-motd.d/01-custom",
+      "sudo chmod +x /home/ubuntu/backup_script.sh",
+      # execute script every day at 23:40 
+      "echo '45 23 * * * /home/ubuntu/backup_script.sh' | crontab"
+      ]
   }
   provisioner "remote-exec" {
     inline = [
       "while [ ! -f /var/lib/cloud/instance/boot-finished ]; do echo 'Waiting for cloud-init...'; sleep 1; done", #very important: help to avoid conflict before apt udates 
       "sudo apt update -y",
-      file("scripts/root_login.sh"),
-      file("scripts/users_login.sh"),
-      "sudo chmod -x /etc/update-motd.d/*",
-      "sudo chmod -x /usr/share/landscape/landscape-sysinfo.wrapper",
-      "sudo chmod +x /etc/update-motd.d/01-custom",
+      # file("scripts/root_login.sh"),
+      # file("scripts/users_login.sh"),
+      # "sudo chmod -x /etc/update-motd.d/*",
+      # "sudo chmod -x /usr/share/landscape/landscape-sysinfo.wrapper",
+      # "sudo chmod +x /etc/update-motd.d/01-custom",
     ]
   }
   provisioner "remote-exec" {
     inline = [
-      "sudo apt install -y inxi screenfetch ansiweather",
+      # "sudo apt install -y inxi screenfetch ansiweather",
       "sudo systemctl restart ssh",
     ]
   }
-  # install docker engine to instance
   provisioner "remote-exec" {
     inline = [
+      # install aws cli and add credentials
+      "sudo apt-get install awscli -y",
+      "aws configure set aws_access_key_id ${var.aws_access_key}",
+      "aws configure set aws_secret_access_key ${var.aws_secret_key}",
+      "aws configure set default.region ${var.aws_region}",
+      # install docker and debploy portfolio-website
       file("scripts/docker_install.sh"),
-      file("scripts/deploy_website.sh")
+      file("scripts/deploy_website.sh"),
+      # create s3 bucket
+      "aws s3api create-bucket --bucket backup-pavlo-test-website --region eu-central-1 --create-bucket-configuration LocationConstraint=eu-central-1",
+      # run backup script
     ]
   }
   connection {
