e37a2eb24ed0bfcfcab45a37242cfd01624b89b7
PavloChaikovskyi <chaykovskiypavel@gmail.com>
2024-01-30 01:15:27 +0100

    feat: Step 5: Associate Public Subnets with the Second Route Table

--- TASK3/main.tf
+++ TASK3/main.tf
@@ -43,6 +43,16 @@ resource "aws_route_table" "second_rt" {
 }
 
 ###################################################################################################
+### ROUTE ASSOTIATIONS
+###################################################################################################
+resource "aws_route_table_association" "public_subnet_asso" {
+ count = length(var.public_subnet_cidrs)
+ subnet_id      = element(aws_subnet.public_subnets[*].id, count.index)
+ route_table_id = aws_route_table.second_rt.id
+}
+
+
+###################################################################################################
 ### PUBLIC SUBNETS
 ###################################################################################################
 resource "aws_subnet" "public_subnets" {
